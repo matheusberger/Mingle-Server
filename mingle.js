1d1f29b7b3c5f60cb9033e56030593be77726c11
Matheus Berger <matheuscberger@gmail.com>
2020-07-13 15:49:29 -0300

    Follow heroku tutorial to deploy it

--- mingle.js
+++ mingle.js
@@ -1,16 +1,22 @@
-var io = require('socket.io')({
-	transports: ['websocket'],
-});
+const express = require('express');
+const socketIO = require('socket.io');
 
-io.attach(4567);
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
 
 console.log("Server running!");
 
 var numOfConnections = 0;
-
 var players = { };
 
 io.on('connection', function (socket) {
+	console.log("client connected at: " + socket.id);
     socket.emit('connect', numOfConnections);
 
     numOfConnections = numOfConnections + 1;
